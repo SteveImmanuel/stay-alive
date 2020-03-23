fe84aef430bdd85671ed84e7545a821a1056c78f
SteveImmanuel <13517039@std.stei.itb.ac.id>
2020-03-24 00:43:43 +0700

    Add shooting mechanism

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float bulletDamage;
+    public GameObject bulletPrefab;
+    public Transform spawnPoint;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
+            Debug.Log("fire pressed");
+            animator.SetBool("isShooting", true);
+            Invoke("stopShooting", .35f);
+        }
+    }
+
+    private void stopShooting()
+    {
+        animator.SetBool("isShooting", false);
+    }
+}
