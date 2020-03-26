63ab34d33bdf2a3cf007f312b94268e6939aa42d
SteveImmanuel <13517039@std.stei.itb.ac.id>
2020-03-26 23:08:55 +0700

    Add wavespawner to spawn enemy randomly

---
+++ Assets/RechargeStation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RechargeStation : MonoBehaviour
+{
+    public float totalEnergy = 10f;
+    public float absorbTime = 1f;
+
+    private ParticleSystem particle;
+    private ParticleSystem.MainModule particleModule;
+    private bool playerInside = false;
+    private float timeSpent = 0;
+
+    void Start()
+    {
+        particle = GetComponentInChildren<ParticleSystem>();
+        particleModule = particle.main;
+    }
+
+    void Update()
+    {
+        if (timeSpent >= absorbTime)
+        {
+            Destroy(gameObject, 2f);
+            particleModule.loop = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerInside)
+        {
+            timeSpent += Time.fixedDeltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("player enter");
+            playerInside = true;
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerInside = false;
+        }
+    }
+}
---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public GameObject[] enemyPrefab;
+    public GameObject rechargeStationPrefab;
+
+    private List<Transform> enemyPoint = new List<Transform>();
+    private List<Transform> energyPoint = new List<Transform>();
+    private int waveIndex = 1;
+
+    void Start()
+    {
+        foreach(Transform child in transform)
+        {
+            if(child.tag == "EnemyPoint")
+            {
+                enemyPoint.Add(child);
+            }else if(child.tag == "EnergyPoint")
+            {
+                energyPoint.Add(child);
+            }
+        }
+
+        Debug.Log("spawning");
+        spawnEnemy();
+        spawnEnergy();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void spawnEnemy()
+    {
+        for(int i = 0; i < getTotalEnemy(); i++)
+        {
+            int enemyPrefabIdx = Random.Range(0, enemyPrefab.Length);
+            int locationIdx = Random.Range(0, enemyPoint.Count);
+            Instantiate(enemyPrefab[enemyPrefabIdx], enemyPoint[locationIdx].position, Quaternion.identity);
+        }
+    }
+
+    private void spawnEnergy()
+    {
+        for(int i=0; i < getTotalEnergy(); i++)
+        {
+            int locationIdx = Random.Range(0, energyPoint.Count);
+            Instantiate(rechargeStationPrefab, energyPoint[locationIdx].position, Quaternion.identity);
+        }
+    }
+
+    private int getTotalEnemy()
+    {
+        int totalEnemy = (int)Mathf.Round(waveIndex * waveIndex * .5f + 5);
+        return Mathf.Clamp(totalEnemy, 6, 13);
+
+    }
+
+    private int getTotalEnergy()
+    {
+        return 3 * waveIndex;
+    }
+}
