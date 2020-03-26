6be862aeeec3c28db10f2a59907c81da90f92b35
SteveImmanuel <13517039@std.stei.itb.ac.id>
2020-03-26 19:12:15 +0700

    Add flashlight mechanism

---
+++ Assets/Scripts/PlayerFlashlight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class PlayerFlashlight : MonoBehaviour
+{
+    public float energy = 100f;
+    public float minRadius = 4f;
+    public float maxRadius = 15f;
+    public float speedFactor = 3;
+
+    private UnityEngine.Experimental.Rendering.Universal.Light2D flashlight;
+    private bool isPowerOn = false;
+
+    void Start()
+    {
+        flashlight = GetComponentInChildren<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            isPowerOn = !isPowerOn;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isPowerOn)
+        {
+            flashlight.pointLightOuterRadius += Time.fixedDeltaTime * speedFactor;
+        }
+        else
+        {
+            flashlight.pointLightOuterRadius -= Time.fixedDeltaTime * speedFactor;
+        }
+        flashlight.pointLightOuterRadius = Mathf.Clamp(flashlight.pointLightOuterRadius, minRadius, maxRadius);
+    }
+}
